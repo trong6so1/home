2afdf595c1ba79d356d6cdacf849842147d11ba0
MAY27\Administrator <trong6so1@gmail.com>
2024-07-29 13:56:47 +0700

    install

--- src/views/template/TheHeader.vue
+++ src/views/template/TheHeader.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <Menubar :model="items">
+      <template #start>
+        <img src="https://dsdhome.vn/images/bggianhang/1626312677_bg.png" />
+      </template>
+    </Menubar>
+  </div>
 </template>
 
 <script>
+import Menubar from 'primevue/menubar'
+import { menuData } from './helpers'
 export default {
-  name: 'TheHeader'
+  name: 'TheHeader',
+  components: {
+    Menubar
+  },
+
+  data() {
+    return {
+      items: null
+    }
+  },
+
+  created() {
+    this.items = menuData
+  }
 }
 </script>
---
+++ src/views/template/helpers.js
@@ -0,0 +1,26 @@
+export const menuData = [
+  {
+    label: 'GIỚI THIỆU'
+  },
+  {
+    label: 'SẢN PHẨM'
+  },
+  {
+    label: 'DỰ ÁN'
+  },
+  {
+    label: 'DỊCH VỤ'
+  },
+  {
+    label: 'TIN TỨC'
+  },
+  {
+    label: 'VIDEO'
+  },
+  {
+    label: 'HỎI ĐÁP'
+  },
+  {
+    label: 'LIÊN HỆ'
+  }
+]
