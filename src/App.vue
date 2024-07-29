0c5691eccd0d2568becfac7b70660fe318461884
MAY27\Administrator <trong6so1@gmail.com>
2024-07-29 12:26:12 +0700

    install

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="nav"></div>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';
+@import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
+    'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+}
+
+body {
+  margin: 0;
+  height: 100%;
+}
+
+html {
+  font-size: 14px;
+}
+
+.swal2-container {
+  z-index: 9999 !important;
+}
+
+:root {
+  --popper-theme-background-color: #333333;
+  --popper-theme-background-color-hover: #333333;
+  --popper-theme-text-color: #ffffff;
+  --popper-theme-border-width: 0px;
+  --popper-theme-border-style: solid;
+  --popper-theme-border-radius: 6px;
+  --popper-theme-padding: 10px;
+  --popper-theme-box-shadow: 0 6px 30px -6px rgba(0, 0, 0, 0.25);
+}
+</style>
---
+++ src/views/template/TheTemplate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheHeader />
+  </div>
+</template>
+
+<script>
+import TheHeader from './TheHeader.vue'
+
+export default {
+  name: 'TheTemplate',
+  components: {
+    TheHeader
+  }
+}
+</script>
